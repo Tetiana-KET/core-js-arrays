1f1b1d699e3a436d2407823dc5a550f379045fcb
tatiana <belangelphone@gmail.com>
2024-01-01 12:56:23 +0200

    createNDimensionalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -273,8 +273,10 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  const arr = new Array(size).fill(0);
+  if (n === 0) return [];
+  return n === 1 ? arr : arr.map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
